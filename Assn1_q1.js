989371d6bc5bdd53021e303aa5e11b206ab766f1
Deepak-iiitg <94068572+Deepak-iiitg@users.noreply.github.com>
2021-12-31 15:09:11 +0530

    Add files via upload

---
+++ Assn1_q1.js
@@ -0,0 +1,7 @@
+var n = prompt();
+if ( n>10 ){
+	console.log("n is greater than 10");
+}
+else{
+	console.log("n is not greter than 10");
+}
---
+++ Assn1_q2.js
@@ -0,0 +1,12 @@
+var a = prompt();
+var b = prompt();
+var c = prompt();
+if ( a>b && a>c ){
+	console.log("a is highest ");
+}
+else if ( b>a && b>c ){
+	console.log("b is highest ");
+}
+else{
+	console.log("c is highest ");
+}
